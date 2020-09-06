9ea266584e06e372f5a44df35b812e57700238f3
Jeressia Williamson <jeressiajaytv@gmail.com>
2020-09-05 21:41:11 -0500

    Initial Commit

---
+++ Tip Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Time
+//
+//  Created by Jeressia Williamson on 9/5/20.
+//  Copyright © 2020 Jeressia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
